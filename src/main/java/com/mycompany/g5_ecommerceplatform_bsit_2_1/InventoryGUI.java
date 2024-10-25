bf4bc4176f5f00c8c3fecc7b8cf661cdd5e69a94
Riyle Lhane Mapanoo <>
2024-10-25 17:56:55 +0800

    Update 1

---
+++ src/main/java/com/mycompany/g5_ecommerceplatform_bsit_2_1/InventoryGUI.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.g5_ecommerceplatform_bsit_2_1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Raylen
+ */
+public class InventoryGUI extends JFrame {
+    public InventoryGUI(){
+    
+       setTitle("ECommercePlatform");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        
+    
+    }
+}
