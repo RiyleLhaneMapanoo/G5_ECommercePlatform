fa5ca358f345bac37d355688da952ef80deb49ea
Sandy <Ostine@DESKTOP-I2KC1NQ>
2024-11-05 17:52:45 +0800

    Cart Page!

--- src/main/java/com/mycompany/g5_ecommerceplatform_bsit_2_1/cartPage.java
+++ src/main/java/com/mycompany/g5_ecommerceplatform_bsit_2_1/cartPage.java
@@ -11,6 +11,9 @@ public class cartPage extends JFrame implements ActionListener {
     
     private JPanel panel, 
             imagePanel;
+    
+    private JPanel[] imgProductPanel;
+    
     private JLabel products;
     JScrollPane scrollPane;
    
@@ -25,10 +28,14 @@ public class cartPage extends JFrame implements ActionListener {
     private String prices[] = {"500", "500", "2000", "1500"};
     private JLabel pPrices;
     
-    private JButton btnMinus, btnAdd, btnDelete;
-    private JLabel pQuantity;
+    private JButton[] btnMinus, btnAdd, btnDelete;
+    private int[] defaultQuantity;
+    private JLabel[] pQuantity;
     
+    private JLabel subTotal;
     
+    private int total = 0;
+    private int pQuantityCount = 0;
     cartPage() {
        
         setTitle("Cart Page");
@@ -36,7 +43,15 @@ public class cartPage extends JFrame implements ActionListener {
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         
+        imgProductPanel = new JPanel[images.length];
         cboxes = new JCheckBox[images.length]; 
+        btnMinus = new JButton[images.length];
+        btnAdd = new JButton[images.length];
+        btnDelete = new JButton[images.length];
+        defaultQuantity = new int[images.length];
+        pQuantity = new JLabel[images.length];
+        
+        
         
        
         
@@ -45,49 +60,53 @@ public class cartPage extends JFrame implements ActionListener {
             ImageIcon pImageIcon = new ImageIcon(images[i]);
             ImageIcon productImages = new ImageIcon(pImageIcon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
             
-    
-            JPanel productPanel = new JPanel();
-            productPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // set the position to the "LEFT", since the default pos is "CENTER"
             
-            cboxes[i] = new JCheckBox();
-            productPanel.add(cboxes[i]);
+            // Panel for the checkboxes and product images
+            imgProductPanel[i] = new JPanel();
+            imgProductPanel[i].setLayout(new FlowLayout(FlowLayout.LEFT)); // set the position to the "LEFT", since the default pos is "CENTER"
+            
+            cboxes[i] = new JCheckBox(); 
+            imgProductPanel[i].add(cboxes[i]);
             
             pImages = new JLabel(productImages);
-            productPanel.add(pImages);
+            imgProductPanel[i].add(pImages);
             
-            // creating another JPanel for the Information(to make the components horizontally aligned)
+            
+            // creating another JPanel for the Information(specifically name and price) to make the components vertically aligned
             JPanel infoPanel = new JPanel();
             infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
             
-            
-            
+
             pNames = new JLabel("Name : "+names[i]);
             infoPanel.add(pNames);
             
             pPrices = new JLabel("Price : "+ Integer.valueOf(prices[i]));
             infoPanel.add(pPrices);
             
-            // add JPanel for the Quantity
+            
+            // add JPanel for the Quantity to make the buttons and label horizontally aligned
             JPanel qPanel = new JPanel();
             qPanel.setLayout(new BoxLayout(qPanel, BoxLayout.X_AXIS));
             
+                   
             //Minus Button
-            btnMinus = new JButton("-");
-            btnMinus.setFocusable(false);
-            qPanel.add(btnMinus);
+            btnMinus[i] = new JButton("-");
+            btnMinus[i].setFocusable(false);
+            qPanel.add(btnMinus[i]);
             
-            qPanel.add(Box.createHorizontalStrut(10));
+            qPanel.add(Box.createHorizontalStrut(10)); // 10px spacing b/w "1" and "+"
             
-            pQuantity = new JLabel("1");
-            qPanel.add(pQuantity);
+            defaultQuantity[i] = 1; // default quantity
+            
+            pQuantity[i] = new JLabel("1");
+            qPanel.add(pQuantity[i]);
             
             qPanel.add(Box.createHorizontalStrut(10)); // 10px spacing b/w "1" and "+"
             
             //Add Button
-            btnAdd = new JButton("+");
-            btnAdd.setFocusable(false);
-        
-            qPanel.add(btnAdd);
+            btnAdd[i] = new JButton("+");
+            btnAdd[i].setFocusable(false);
+            qPanel.add(btnAdd[i]);
             
             qPanel.add(Box.createHorizontalStrut(10));  // 10px spacing b/w "+" and "Delete"
             
@@ -95,42 +114,78 @@ public class cartPage extends JFrame implements ActionListener {
             //Delete Button
             ImageIcon deleteIcon = new ImageIcon("deleteIcon.png");
             ImageIcon deleteImage = new ImageIcon(deleteIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
-            btnDelete = new JButton(deleteImage);
-            btnDelete.setBackground(Color.red);
-            btnDelete.setFocusable(false);
+            btnDelete[i] = new JButton(deleteImage);
+            btnDelete[i].setBackground(Color.red);
+            btnDelete[i].setFocusable(false);
             
-            qPanel.add(btnDelete);
+            qPanel.add(btnDelete[i]);
             
             JPanel productDetails = new JPanel();
             productDetails.setLayout(new FlowLayout(FlowLayout.LEFT));
             productDetails.add(infoPanel);
             productDetails.add(qPanel);
             
+            imgProductPanel[i].add(productDetails);
+            panel.add(imgProductPanel[i]);
             
-            // Panel for subtotal
-//            JPanel subTotalPanel = new JPanel();
-//            subTotalPanel.setBackground(Color.CYAN);
-//            panel.add(subTotalPanel);
-            
-            
-            
-            productPanel.add(productDetails);
-            panel.add(productPanel);
+            // Adding ActionListener
+            cboxes[i].addActionListener(this);
+            btnAdd[i].addActionListener(this);
+            btnMinus[i].addActionListener(this);
+            btnDelete[i].addActionListener(this);
         }
         
+        
+        
+         // Panel for Back button, Subtotal and Checkout button
+        
+            JPanel botPanel = new JPanel();
+            botPanel.setLayout(new BoxLayout(botPanel, BoxLayout.X_AXIS));
+        
+            
+            
+                     
+        // Back Button
+        
+            ImageIcon icon = new ImageIcon("back.png");
+            ImageIcon backIcon = new ImageIcon(icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
+            JButton btnBack = new JButton(backIcon);
+            btnBack.setBackground(Color.PINK);
+            
+            botPanel.add(btnBack);
+            
+            
+        // Subtotal
+        
             JPanel subTotalPanel = new JPanel();
-            
-            
-            JLabel lblsubTotal = new JLabel("Subtotal : ");
-            subTotalPanel.add(lblsubTotal);
-            
-            JLabel subTotal = new JLabel("0");
+        
+            JLabel lblSubTotal = new JLabel("Subtotal : ");
+            subTotalPanel.add(lblSubTotal);
+            subTotal = new JLabel("0");
             subTotalPanel.add(subTotal);
+            
+            botPanel.add(subTotalPanel);
+            
+            
+        // Checkout
+        
+            JButton btnCheckOut = new JButton("Checkout");
+            btnCheckOut.setBackground(Color.PINK);
+            btnCheckOut.setFocusable(false);
+            
+            botPanel.add(btnCheckOut);
+            
+   
+            
+        
+       
           
             scrollPane = new JScrollPane(panel);
             getContentPane().add(scrollPane, BorderLayout.CENTER);
-            getContentPane().add(subTotalPanel, BorderLayout.SOUTH);
+            getContentPane().add(botPanel, BorderLayout.SOUTH);
             
+        
+        
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -142,7 +197,32 @@ public class cartPage extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        total = 0; // reset total
+        
+        for(int i = 0; i < cboxes.length; i++) {
        
+            if(e.getSource() == btnAdd[i]) {
+                defaultQuantity[i]++;
+                pQuantity[i].setText(String.valueOf(defaultQuantity[i]));
+            }
+            else if(e.getSource() == btnMinus[i]) {
+                if(defaultQuantity[i] > 1) {
+                    defaultQuantity[i]--;
+                    pQuantity[i].setText(String.valueOf(defaultQuantity[i]));
+                }
+            }
+            else if(e.getSource() == btnDelete[i]) {
+                
+                imgProductPanel[i].setVisible(false);
+                cboxes[i].setSelected(false);
+            }
+            
+             if(cboxes[i].isSelected()) {
+                total+= Integer.parseInt(prices[i])* defaultQuantity[i];   
+            }
+        }
+        
+        subTotal.setText(String.valueOf(total));
         
     }
 
