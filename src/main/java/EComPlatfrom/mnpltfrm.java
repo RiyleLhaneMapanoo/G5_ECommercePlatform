d4242731790a3a6787088d54372d963f386c699e
Elaine Joy Mendoza <>
2024-11-06 16:34:52 +0800

    package transfer

--- src/main/java/EComPlatfrom/mnpltfrm.java
+++ src/main/java/EComPlatfrom/mnpltfrm.java
@@ -634,9 +634,4 @@ public class mnpltfrm extends JFrame {
 
     setVisible (true);
     }
-
-     public static void main(String[] args) {
-        SwingUtilities.invokeLater(mnpltfrm::new);
-    }
-
 }
