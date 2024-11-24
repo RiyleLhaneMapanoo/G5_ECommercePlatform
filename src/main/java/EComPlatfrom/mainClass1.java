219f3c618d3ecb6b04e6e787b40d6b9dc8bf3d54
Riyle Lhane Mapanoo <Raylen@DESKTOP-NAC05LQ>
2024-11-25 01:18:51 +0800

    Current Test Packages Update 5

---
+++ src/main/java/EComPlatfrom/mainClass1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EComPlatfrom;
+
+
+
+/**
+ *
+ * @author Raylen
+ */
+public class mainClass1 {
+    public static void main(String[] args){
+    
+    LoginPage login = new LoginPage();
+    login.setVisible(true);
+    login.setResizable(false);
+   
+    
+    }
+}
---
+++ src/main/java/EComPlatfrom/mainClass2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EComPlatfrom;
+
+/**
+ *
+ * @author Raylen
+ */
+public class mainClass2 {
+
+    public static void main(String[] args) {
+
+        eComPageSeller login = new eComPageSeller();
+        login.setVisible(true);
+        login.setResizable(false);
+        // login. setLocationRelativeTo(null);
+
+    }
+}
