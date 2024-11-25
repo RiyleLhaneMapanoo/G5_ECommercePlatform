56dffd0014d12870fec0f15386d01e0371e83b3a
Riyle Lhane Mapanoo <Raylen@DESKTOP-NAC05LQ>
2024-11-25 17:07:21 +0800

    Merge origin/master
    # Conflicts:
    #	pom.xml
    #	src/main/java/EComPlatfrom/mainClass1.java

--- src/main/java/EComPlatfrom/mainClass1.java
+++ src/main/java/EComPlatfrom/mainClass1.java
@@ -13,9 +13,7 @@ package EComPlatfrom;
 public class mainClass1 {
     public static void main(String[] args){
     
-   // LoginPage login = new LoginPage();
-   // login.setVisible(true);
-   // login.setResizable(false);
+
    
     
     }
