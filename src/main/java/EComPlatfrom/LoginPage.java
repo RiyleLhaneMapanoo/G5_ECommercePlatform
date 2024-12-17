f05b21d87fd6780e63039c9cdc0054186032f0dd
chamer <>
2024-12-17 12:51:19 +0800

    tEST UPDATE COMMIT 3

--- src/main/java/EComPlatfrom/LoginPage.java
+++ src/main/java/EComPlatfrom/LoginPage.java
@@ -27,7 +27,7 @@ public class LoginPage extends JFrame implements ActionListener {
     JPanel plBackround;
     //TEST UPDte
     //for user class:
-    UserClass userClass = new UserClass();//hi
+   //what
     
     LoginPage(){
         
@@ -119,7 +119,7 @@ public class LoginPage extends JFrame implements ActionListener {
             
             String strEmail = txfEmail.getText();
             String strPassword = new String(pfPassword.getPassword());
-            userClass.loginMethod(LoginPage.this,strEmail, strPassword, txfEmail, pfPassword);
+            //userClass.loginMethod(LoginPage.this,strEmail, strPassword, txfEmail, pfPassword);
             
           
         }
