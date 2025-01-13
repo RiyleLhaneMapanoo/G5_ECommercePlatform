e12c86d93af11c87daa3c015fe273ecb038a8604
Aliyah <Maglonso@DESKTOP-D163VQD>
2025-01-13 15:53:24 -0800

    updated add product gui

--- src/main/java/EComPlatfrom/addProduct.java
+++ src/main/java/EComPlatfrom/addProduct.java
@@ -14,7 +14,7 @@ public class addProduct extends JFrame implements ActionListener{
      private UserClass userClass;
    private int userId; 
    private JTextArea txaHistory;
-   private JLabel lblHistory, lblIcon, lblTitle, lblup, lblPname, lblPprice, lblPrating, lblPcategory, lblSA;
+   private JLabel lblHistory, lblIcon, lblTitle, lblup, lblPname, lblPprice, lblPrating, lblPcategory, lblSA, bgLabel;
    private JScrollPane spTable;
    private JTable ohtable;
    private JPanel plist;
@@ -41,85 +41,82 @@ public class addProduct extends JFrame implements ActionListener{
         setLocationRelativeTo(null);
         setContentPane(new JLabel(new ImageIcon("src\\main\\java\\Images\\sellerBackground.png")));
         
+        ImageIcon bgIcon = new ImageIcon("src\\main\\java\\Images\\addprdbgi.png");
+        bgLabel = new JLabel(bgIcon);
+
+        Image scaledImage = bgIcon.getImage().getScaledInstance(1385,764, Image.SCALE_SMOOTH);
+        
+        bgLabel.setIcon(new ImageIcon(scaledImage));
+        bgLabel.setBounds(0, 0, 1385, 764);
+        add(bgLabel);
+        
         lblTitle = new JLabel("ADD PRODUCT");
         lblTitle.setBounds(535, 20, 350, 50);
         lblTitle.setFont(new Font("Arial",Font.BOLD,45));
-        add(lblTitle);
+        bgLabel.add(lblTitle);
         
         lblPname = new JLabel("PRODUCT NAME:");
         lblPname.setBounds(80, 150, 250, 40);
         lblPname.setFont(new Font("Arial",Font.BOLD,18));
-        add(lblPname);
+        bgLabel.add(lblPname);
         
         txfPname = new JTextField();
         txfPname.setBounds(255, 155, 250, 30);
         txfPname.setFont(new Font("Arial",Font.BOLD,16));
-        add(txfPname);
-        
+        bgLabel.add(txfPname);
+          
         lblPprice = new JLabel("PRODUCT PRICE:");
         lblPprice.setBounds(80, 230, 250, 40);
         lblPprice.setFont(new Font("Arial",Font.BOLD,18));
-        add(lblPprice);
+        bgLabel.add(lblPprice);
         
         txfPprice = new JTextField();
         txfPprice.setBounds(255, 235, 250, 30);
         txfPprice.setFont(new Font("Arial",Font.BOLD,16));
-        add(txfPprice);
+        bgLabel.add(txfPprice);
         
         lblPcategory = new JLabel("PRODUCT CATEGORY:");
         lblPcategory.setBounds(80, 310, 250, 40);
         lblPcategory.setFont(new Font("Arial",Font.BOLD,18));
-        add(lblPcategory);
+        bgLabel.add(lblPcategory);
         
         txfPcategory = new JComboBox(Pcategory);
-        txfPcategory.setBounds(300, 315, 250, 30);
+        txfPcategory.setBounds(280, 315, 250, 30);
         lblPcategory.setFont(new Font("Arial",Font.BOLD,16));
-        add(txfPcategory);
+        bgLabel.add(txfPcategory);
         
         lblPrating = new JLabel("PRODUCT RATING:");
         lblPrating.setBounds(80, 390, 250, 40);
         lblPrating.setFont(new Font("Arial",Font.BOLD,18));
-        add(lblPrating);
+        bgLabel.add(lblPrating);
         
         txfPrating = new JComboBox(PR);
         txfPrating.setBounds(275, 395, 250, 30);
         txfPrating.setFont(new Font("Arial",Font.BOLD,16));
-        add(txfPrating);
+        bgLabel.add(txfPrating);
         
         lblSA = new JLabel("STOCK AVAILABLE:");
         lblSA.setBounds(80, 470, 270, 50);
         lblSA.setFont(new Font("Arial",Font.BOLD,18));
-        add(lblSA);
+        bgLabel.add(lblSA);
         
         txfSA = new JTextField();
         txfSA.setBounds(285, 475, 250, 30);
         txfSA.setFont(new Font("Arial",Font.BOLD,16));
-        add(txfSA);
+        bgLabel.add(txfSA);
         
         btnBack =  new JButton("Back");
-        btnBack.setBounds(780, 570, 144, 40);
+        btnBack.setBounds(780, 150, 144, 55);
         btnBack.setFont(new Font("Arial",Font.BOLD,17));
-        add(btnBack);
-        
-        btnup =  new JButton ("Upload Photo");
-        btnup.setBounds(970, 570, 155, 40);
-        btnup.setFont(new Font("Arial",Font.BOLD,17));
-        add(btnup);
-        
-      lblup =  new JLabel ("************Picture************");
-        lblup.setBounds(970, 580, 155, 37);
-        lblup.setFont(new Font("Arial",Font.BOLD,12));
-        add(lblup);
-        
-       
+        bgLabel.add(btnBack);                                    
         
         btnAddP =  new JButton("Add Product");
-        btnAddP.setBounds(870, 650, 155, 40);
+        btnAddP.setBounds(780, 250, 155, 55);
         btnAddP.setFont(new Font("Arial",Font.BOLD,17));
-        add(btnAddP);
+        bgLabel.add(btnAddP);
         
         btnBack.addActionListener(this);
-        btnAddP.addActionListener(this);
+
     }
 
     @Override
@@ -133,36 +130,6 @@ public class addProduct extends JFrame implements ActionListener{
              dispose();
    
         
-       }else if(e.getSource()==btnAddP){
-           
-           try{
-        String productName = txfPname.getText();
-        double productPrice = Double.parseDouble(txfPprice.getText()); 
-        int stockAvail = Integer.parseInt(txfSA.getText());
-        int prRating = Integer.parseInt((String) txfPrating.getSelectedItem()); 
-        String prCategory = (String) txfPcategory.getSelectedItem(); 
-        
-          pr.addProduct(btnAddP,productName,productPrice,stockAvail,prRating,prCategory,txfPname,txfPprice,txfSA,txfPrating, txfPcategory);
-        
-           }catch(NumberFormatException ex){
-           
-           JOptionPane.showMessageDialog(null, "Please ensure all fields are correctly filled in with their appropriate info. ", "Input Error", JOptionPane.ERROR_MESSAGE);
-            txfPname.setText("");
-                txfPprice.setText("");
-                txfSA.setText("");
-                txfPrating.setSelectedIndex(0);
-               txfPcategory.setSelectedIndex(0);
-           
-           }
-         
-           
-         
-          
-          
-        
-            }
-    
-       
-       
+       }
     }
 }
